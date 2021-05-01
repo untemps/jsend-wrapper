f1bdad08736d96ef6faf996ff5f64c064e2fd855
untemps <6Mv2^2S6$2>
2021-05-01 15:37:31 +0200

    chore(bundle): Export types and enums

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export { default as JSendWrapper } from './JSendWrapper'
+export { default as TJSendResponse } from './JSendResponse.type'
+export { default as EJSendStatus } from './JSendStatus.enum'
