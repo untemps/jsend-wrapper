fc53815da305cff3ee1096d624241ecbac51221f
untemps <6Mv2^2S6$2>
2021-05-01 19:34:03 +0200

    feat: Return status code in place of error code if not provided

--- src/__tests__/JSendWrapper.test.ts
+++ src/__tests__/JSendWrapper.test.ts
@@ -11,16 +11,15 @@ describe('JSendWrapper', () => {
 		[300, 'foo', undefined, undefined, { status: 'success', data: 'foo' }],
 		[400, 'foo', undefined, undefined, { status: 'fail', data: 'foo' }],
 		[499, 'foo', undefined, undefined, { status: 'fail', data: 'foo' }],
-		[500, 'foo', undefined, undefined, { status: 'error', message: 'foo' }],
-		[500, ['foo'], undefined, undefined, { status: 'error', message: ['foo'] }],
+		[500, 'foo', undefined, undefined, { status: 'error', message: 'foo', code: 500 }],
+		[500, ['foo'], undefined, undefined, { status: 'error', message: ['foo'], code: 500 }],
 		[200, 42, undefined, undefined, { status: 'success', data: 42 }],
 		[200, undefined, undefined, undefined, { status: 'success', data: null }],
-		[500, undefined, undefined, undefined, { status: 'error', message: 'Error' }],
-		[500, 'foo', undefined, undefined, { status: 'error', message: 'foo' }],
-		[500, 'foo', undefined, undefined, { status: 'error', message: 'foo' }],
+		[500, undefined, undefined, undefined, { status: 'error', message: 'Error', code: 500 }],
+		[500, 'foo', undefined, undefined, { status: 'error', message: 'foo', code: 500 }],
 		[500, 'foo', 12, undefined, { status: 'error', message: 'foo', code: 12 }],
 		[500, 'foo', 12, 'bar', { status: 'error', message: 'foo', code: 12, data: 'bar' }],
-		[500, 'foo', undefined, 'bar', { status: 'error', message: 'foo', data: 'bar' }],
+		[500, 'foo', undefined, 'bar', { status: 'error', message: 'foo', code: 500, data: 'bar' }],
 	])('should wrap the response in the right format', (statusCode, data, errorCode, errorData, expected) => {
 		expect(new JSendWrapper().wrap(statusCode, data, errorCode, errorData)).toEqual(expected)
 	})
